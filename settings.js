23ce95f0781f8dc87cff1d63665ea8a9cc81cadc
Horlardc010 <ramotallahiayoade13@gmail.com>
2025-01-04 03:16:00 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "OPAY"
 
-global.banknumber = "8133729715"
+global.banknumber = "6112719990"
 
-global.bankowner = "FADARE"
+global.bankowner = "AYOADE RAMOTA"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
